9ea165679afc27331ad0ac45f70fe1778af912cf
xhy940801 <xhyxzg@gmail.com>
2015-02-01 13:10:50 +0800

    init version

---
+++ labelsys.sql
@@ -0,0 +1,197 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: 2015-02-01 12:16:47
+-- 服务器版本： 5.6.17-log
+-- PHP Version: 5.4.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `labelsys`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `entities`
+--
+
+CREATE TABLE IF NOT EXISTS `entities` (
+`id` int(11) NOT NULL,
+  `system_id` int(11) NOT NULL,
+  `module_id` int(11) NOT NULL,
+  `foreign_key` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `entities_labels`
+--
+
+CREATE TABLE IF NOT EXISTS `entities_labels` (
+`id` int(11) NOT NULL,
+  `entity_id` int(11) NOT NULL,
+  `label_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `labels`
+--
+
+CREATE TABLE IF NOT EXISTS `labels` (
+`id` int(11) NOT NULL,
+  `system_id` int(11) NOT NULL,
+  `module_id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `modules`
+--
+
+CREATE TABLE IF NOT EXISTS `modules` (
+`id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `operations`
+--
+
+CREATE TABLE IF NOT EXISTS `operations` (
+`id` int(11) NOT NULL,
+  `system_id` int(11) NOT NULL,
+  `module_id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `rules`
+--
+
+CREATE TABLE IF NOT EXISTS `rules` (
+`id` int(11) NOT NULL,
+  `operation_id` int(11) NOT NULL,
+  `exp` varchar(255) COLLATE utf8_bin NOT NULL,
+  `permission` int(11) NOT NULL,
+  `created` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `systems`
+--
+
+CREATE TABLE IF NOT EXISTS `systems` (
+`id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `entities`
+--
+ALTER TABLE `entities`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `system_id` (`system_id`,`module_id`,`foreign_key`);
+
+--
+-- Indexes for table `entities_labels`
+--
+ALTER TABLE `entities_labels`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `entity_id` (`entity_id`,`label_id`);
+
+--
+-- Indexes for table `labels`
+--
+ALTER TABLE `labels`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `system_id` (`system_id`,`module_id`,`name`);
+
+--
+-- Indexes for table `modules`
+--
+ALTER TABLE `modules`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `operations`
+--
+ALTER TABLE `operations`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `system_id` (`system_id`,`module_id`,`name`);
+
+--
+-- Indexes for table `rules`
+--
+ALTER TABLE `rules`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `systems`
+--
+ALTER TABLE `systems`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `name` (`name`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `entities`
+--
+ALTER TABLE `entities`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `entities_labels`
+--
+ALTER TABLE `entities_labels`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `labels`
+--
+ALTER TABLE `labels`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `modules`
+--
+ALTER TABLE `modules`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `operations`
+--
+ALTER TABLE `operations`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `rules`
+--
+ALTER TABLE `rules`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `systems`
+--
+ALTER TABLE `systems`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
